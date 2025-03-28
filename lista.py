813b60f59152c2a0889f103f0493fb991b5949be
ayauribe <ayauribe005@gmail.com>
2025-03-27 21:45:14 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,6 @@
+lista = ['MARIA', 'PAULO', 'JOAO', 'PEDRO', 'MARTA']
+
+for nome in lista:
+    print(f"Olá {nome}, tudo bem?")
+    
+    
---
+++ tabuada.py
@@ -0,0 +1,8 @@
+num = 2
+while num <= 10:
+    i = 1
+    while i <= 10:
+        print(f"{num} x {i} = {num * i} ")
+        i += 1
+    num += 1
+    print("")
